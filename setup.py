394f61b388695577235b0560ccc5c998ae3bd7fe
Niklas Saari <niklas.saari@tutanota.com>
2022-07-18 18:05:10 +0300

    Include submodules on packaging, change name to more import friendly

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='python-markdown-generator',
+setup(name='markdowngenerator',
       version='0.1',
       description='Python library for dynamically generating HTML sanitized Markdown syntax.',
       long_description=open('README.md').read(),
@@ -8,5 +8,5 @@ setup(name='python-markdown-generator',
       author='Niklas Saari',
       author_email='niklas.saari@oulu.fi',
       license='Apache-2.0',
-      packages=['markdowngenerator'],
+      packages=['markdowngenerator', 'markdowngenerator.config'],
       zip_safe=False)
